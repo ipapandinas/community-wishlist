0ec89df5a9bf0a10b1c4e245d0400699cedfd4b3
Igor Papandinas <26460174+ipapandinas@users.noreply.github.com>
2024-04-23 12:18:53 +0200

    format

--- config/site.ts
+++ config/site.ts
@@ -1,6 +1,6 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-	name: "Community Wishes",
-	description: "Suggest ideas for your favorite project to implement.",
+  name: "Community Wishes",
+  description: "Suggest ideas for your favorite project to implement.",
 };
