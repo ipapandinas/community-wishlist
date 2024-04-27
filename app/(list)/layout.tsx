69c19a95267bd4f708edd5e2e1364b00b6f48851
Igor Papandinas <26460174+ipapandinas@users.noreply.github.com>
2024-04-27 11:43:59 +0200

    more minor tweaks

--- app/(list)/layout.tsx
+++ app/(list)/layout.tsx
@@ -4,7 +4,6 @@ import Tabs from "@/components/wishlist/tabs";
 import { Button } from "@nextui-org/button";
 import { Link } from "@nextui-org/link";
 import { TernoaIcon } from "@/components/icons";
-import { Chip } from "@nextui-org/chip";
 
 export default function WishlistLayout({
   children,
@@ -13,10 +12,10 @@ export default function WishlistLayout({
 }) {
   return (
     <>
-      <section className="text-center mx-auto max-w-xl mt-16">
+      <section className="text-center mx-auto max-w-xl mt-8">
         <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
           <TernoaIcon className="w-80" width={40} height={40} />
-          <h1 className="tracking-tight inline font-semibold text-[2.5rem] lg:text-5xl">
+          <h1 className="tracking-tight inline font-semibold text-[2.5rem] leading-tight lg:text-5xl">
             Top{" "}
             <span className="bg-clip-text text-transparent bg-gradient-to-b from-[#FF1CF7] to-[#b249f8]">
               wishes
@@ -24,8 +23,10 @@ export default function WishlistLayout({
             : Curated by the Ternoa Community and You
           </h1>
           <h2 className={subtitle({ class: "mt-4" })}>
-            Imagine the possibilities with Ternoa. Suggest protocol improvements
-            or new apps to be build — every idea counts!
+            Imagine the possibilities with Ternoa. Suggest & Vote protocol
+            improvements or new apps to be build —{" "}
+            <span className="underline">No account needed</span>, every feedback
+            matters!
           </h2>
         </div>
         <Link className="mt-4" href="/new-wish">
--- components/wishlist/tabs.tsx
+++ components/wishlist/tabs.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import React, { useRef, useState } from "react";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { Tabs as NuiTabs, Tab } from "@nextui-org/tabs";
 import { Chart, Fire, Stars } from "../icons";
@@ -8,6 +9,8 @@ export default function Tabs() {
   const router = useRouter();
   const pathname = usePathname();
   const searchParams = useSearchParams();
+  const topDivRef = useRef<HTMLDivElement>(null);
+  const [isReady, setIsReady] = useState(false);
 
   const handleSelectionChange = (selection: unknown) => {
     const value = selection as string;
@@ -15,47 +18,56 @@ export default function Tabs() {
     current.set("sort", value);
     const search = current.toString();
     const query = search ? `?${search}` : "";
-    router.replace(`${pathname}${query}`);
+    router.replace(`${pathname}${query}`, { scroll: false });
+    if (topDivRef.current && isReady) {
+      topDivRef.current.scrollIntoView({ behavior: "smooth" });
+    }
     router.refresh();
+    if (!isReady) {
+      setIsReady(true);
+    }
   };
 
   return (
-    <div className="flex w-full flex-col sticky top-2 pt-4 bg-background-dark z-10">
-      <NuiTabs
-        classNames={{ tabList: "mx-auto sm:mx-0 !bg-black" }}
-        aria-label="Sorting"
-        color="primary"
-        variant="bordered"
-        onSelectionChange={handleSelectionChange}
-      >
-        <Tab
-          key="hot"
-          title={
-            <div className="flex items-center space-x-2">
-              <Fire />
-              <span>Hot</span>
-            </div>
-          }
-        />
-        <Tab
-          key="new"
-          title={
-            <div className="flex items-center space-x-2">
-              <Stars />
-              <span>New</span>
-            </div>
-          }
-        />
-        <Tab
-          key="top"
-          title={
-            <div className="flex items-center space-x-2">
-              <Chart />
-              <span>Top</span>
-            </div>
-          }
-        />
-      </NuiTabs>
-    </div>
+    <>
+      <div ref={topDivRef} />
+      <div className="flex w-full flex-col sticky top-2 pt-4 bg-background-dark z-10">
+        <NuiTabs
+          classNames={{ tabList: "mx-auto sm:mx-0 !bg-black" }}
+          aria-label="Sorting"
+          color="primary"
+          variant="bordered"
+          onSelectionChange={handleSelectionChange}
+        >
+          <Tab
+            key="hot"
+            title={
+              <div className="flex items-center space-x-2">
+                <Fire />
+                <span>Hot</span>
+              </div>
+            }
+          />
+          <Tab
+            key="new"
+            title={
+              <div className="flex items-center space-x-2">
+                <Stars />
+                <span>New</span>
+              </div>
+            }
+          />
+          <Tab
+            key="top"
+            title={
+              <div className="flex items-center space-x-2">
+                <Chart />
+                <span>Top</span>
+              </div>
+            }
+          />
+        </NuiTabs>
+      </div>
+    </>
   );
 }
